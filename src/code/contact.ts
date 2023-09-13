4e5678fa62bc681e09ac5c77e8ca8dc0eeb37e41
Eric Labashosky <elabashosky@hsdyn.com>
2023-09-13 10:06:57 -0400

    code clean up

--- src/code/contact.ts
+++ src/code/contact.ts
@@ -1,16 +1,18 @@
-﻿import EventContext = Xrm.Events.EventContext;
+﻿/* eslint no-unused-vars: 0 */
+/* eslint @typescript-eslint/no-unused-vars: 0 */
+import EventContext = Xrm.Events.EventContext;
 import FormContext = Xrm.FormContext;
 import OptionSetAttribute = Xrm.Attributes.OptionSetAttribute;
 import StandardControl = Xrm.Controls.StandardControl;
 import { setDisabled, displayAlert } from "./common"; 
 import {ROLES} from "./common";
 
-var FIELD = {
+const FIELD = {
 	firstName: 'firstname',
 }
 
 export function onLoad(context: EventContext) {
-	var formContext = context.getFormContext();
+	const formContext = context.getFormContext();
 	setDisabled(formContext, FIELD.firstName, true);
 	displayAlert("Hey", "Hello World", "Ok");
 }
