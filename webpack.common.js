aec27b7ca6709f7c0559460202206029a45bbd32
Eric Labashosky <elabashosky@hsdyn.com>
2024-06-18 08:59:37 -0400

    Adds CopyWebPackPlugin to move assets to the publisher output directory

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,7 @@
+const publisher = 'abc';
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 var glob = require("glob");
 
 function getTypeScriptEntryPoints() {
@@ -11,31 +13,36 @@ function getTypeScriptEntryPoints() {
     return entry;
 }
 
-module.exports = {
-    devtool: 'source-map',
-    entry: getTypeScriptEntryPoints(),
-    output: {
-        filename: '[name].js',
-        sourceMapFilename: 'maps/[name].js.map',
-        path: path.resolve(__dirname, './abc_/scripts'),
-        library: ['abc', '[name]'],
-        libraryTarget: 'var'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
+module.exports = 
+    {
+        name: 'code',
+        devtool: 'source-map',
+        entry: getTypeScriptEntryPoints(),
+        output: {
+            filename: '[name].js',
+            sourceMapFilename: 'maps/[name].js.map',
+            path: path.resolve(__dirname, `./${publisher}_/scripts`),
+            library: [publisher, '[name]'],
+            libraryTarget: 'var'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new CopyPlugin({
+                patterns: [{from: "src/assets", to: `${publisher}_/../assets` }],
+              }),
         ],
-    },
-    plugins: [
-        new CleanWebpackPlugin()
-    ],
-    resolve: {
-        extensions: ['.ts', '.js'],
-    }
-};
+        resolve: {
+            extensions: ['.ts', '.js'],
+        }
+    };
 
 
